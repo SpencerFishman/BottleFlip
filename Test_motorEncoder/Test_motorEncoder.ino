b892ffa0fc8684080654e2c3092b4b496899d9b4
Spencer Fishman <spencer@fibbol.com>
2017-05-23 17:29:28 -0400

    Use PC Repo

--- Test_motorEncoder/Test_motorEncoder.ino
+++ Test_motorEncoder/Test_motorEncoder.ino
@@ -1,5 +1,4 @@
 // C++ Includes
-#include <vector>
 #include <stdlib.h> 
 
 // Custom C++ Includes
@@ -15,12 +14,13 @@
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 Adafruit_DCMotor *motor1 = AFMS.getMotor(1);
 
-// PID
-//std::vector<PID*> pids;
+// Motor
 PID *pid;
 
 
 long dt;
+long pods[4];
+bool up;
 
 void setup() {
   Serial.begin(9600);
@@ -28,10 +28,17 @@ void setup() {
   AFMS.begin();
   
   Encoders::beginEnc1(2,4,3,5);
-  pid = new PID(8000,0.25,0,7000);
+  pid = new PID(-2500,0.35,0,0);
   pid->oMax = 255;
   pid->oMin = -255;
   dt=1;
+
+  pods[0] = 0;
+  pods[1] = 0;
+  pods[2] = 0;
+  pods[3] = 0;
+
+  up = true;
 }
 
 void loop() {
@@ -49,6 +56,23 @@ void loop() {
   else
     motor1->run(FORWARD);
   motor1->setSpeed(abs(cO));
+  if (pods[0] == Encoders::encoder(1)->position && (pods[0] <= -2000 || up == false) ) {
+    pods[0] = 0;
+    pods[1] = 0;
+    pods[2] = 0;
+    pods[3] = 0;
+
+    if (up)
+      pid->sp = 0;
+    else
+      pid->sp = -2500;
+    up = !up;
+  }
+
+  pods[0] = pods[1];
+  pods[1] = pods[2];
+  pods[2] = pods[3];
+  pods[3] = Encoders::encoder(1)->position;
 }
 
 
